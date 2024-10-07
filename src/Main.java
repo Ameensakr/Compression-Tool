b43d6041849d72e6b01c7d23edc20a7b8d788eda
Ameensakr <ameensakr623@gmail.com>
2024-10-07 21:14:08 +0300

    step 0&1 done

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    static File file;
+    static String fileName;
+    static String cwd = new String(System.getProperty("user.dir") + '/');
+    static HashMap<Character , Integer> freq = new HashMap<Character, Integer>();
+
+    static boolean isTxtFile()
+    {
+        if(fileName.length() < 4)return false;
+        String target = ".txt";
+        int idx = 0;
+        for (int i = fileName.length() - 4 ; i < fileName.length(); i++)
+        {
+            if((fileName.charAt(i)) != (target.charAt(idx)))return false;
+            idx++;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to my compress tool");
+
+        boolean validInput = false;
+        Scanner input = new Scanner(System.in);
+        while(!validInput)
+        {
+            System.out.println("you want to compress or decompress [C / D] ");
+            char CD =  input.next().charAt(0);
+
+
+            if(CD != 'C' && CD != 'D')
+            {
+                System.out.println("Please Enter a valid input!");
+            }
+            else validInput = true;
+        }
+
+        input.nextLine();
+
+
+        validInput = false;
+        while(!validInput)
+        {
+            System.out.println("Please Enter the text file name : ");
+            fileName = new String(input.nextLine());
+
+            if(isTxtFile())
+            {
+                validInput = true;
+            }
+            else {
+                System.out.println("please Enter ((TEXT)) File! ");
+            }
+        }
+
+        try
+        {
+            file = new File(cwd + fileName);
+            FileInputStream fis = new FileInputStream(file);
+            int r=0;
+            while((r=fis.read())!=-1)
+            {
+                freq.put((char)r , freq.getOrDefault((char)r , 0) + 1);
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
